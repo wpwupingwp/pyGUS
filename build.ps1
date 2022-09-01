578ffd92f6f7c1a6f2fd26d660e17cdbe640c5f0
Ping Wu <wpwupingwp@outlook.com>
2022-09-01 15:18:47 +0800

    add icon

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 venv/Scripts/activate.ps1
-pip install Nuitka==1.0.4
+pip install Nuitka==1.0.6
+pip install imageio
 pip install orderedset
-nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy pyGUS
+nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy pyGUS --windows-icon-from-ico=icon.png
 # mac
 # nuitka3 --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy pyGUS
