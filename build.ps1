721e932184f02e75f4bd22679c65f3b72f5f0771
Ping Wu <wpwupingwp@outlook.com>
2022-11-10 11:50:36 +0800

    use pyinstaller to build

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 # currently powershell should use 7.2
-venv/Scripts/activate.ps1
-pip install Nuitka==1.0.7
-pip install imageio
-pip install orderedset
-nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy pyGUS --windows-icon-from-ico=icon.png
+pyinstaller pyGUS/__main__.py -c -F -n pygus1.3
+#venv/Scripts/activate.ps1
+#pip install Nuitka==1.0.7
+#pip install imageio
+#pip install orderedset
+#nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy pyGUS --windows-icon-from-ico=icon.png
 # mac
 # nuitka3 --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy pyGUS
