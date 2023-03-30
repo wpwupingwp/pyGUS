7d06ce596f024c9bbfc091b339436cb0919ec032
Ping Wu <wpwupingwp@outlook.com>
2023-03-30 18:25:24 +0800

    apply negative adjustment

--- stats.py
+++ stats.py
@@ -1,31 +1,68 @@
 #!/usr/bin/python3
 from sys import argv
 import csv
-import cv2
-from matplotlib import pyplot as plt
 
-data = dict()
-with open(argv[1], 'r', newline='') as _:
-    reader = csv.reader(_)
-    # filename, sample, group
-    for row in reader:
-        filename, sample, group = row
-        data[sample] = [filename, group]
-        img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
-        if img is None:
-            raise ValueError(f'Bad input file {filename}')
-        b, g, r, a = cv2.split(img)
-        #whole[whole>0] = 255
-        # b = cv2.bitwise_and(b, whole)
-        # g = cv2.bitwise_and(g, whole)
-        # r = cv2.bitwise_and(r, whole)
-        # merge = cv2.merge([b, g, r])
-        b[a<255] = 255
-        b_r = 255 - b
-        v = plt.violinplot(b_r[a>0], showmeans=False, showmedians=False, showextrema=False)
-        plt.show()
-        cv2.imshow('raw', img)
-        # cv2.imshow('2', b_r)
-        cv2.waitKey()
-        break
-print(data.items(), sep='\n')
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+
+def get_sample_info(csv_file: str) -> dict:
+    # sample: (filename, group)
+    data = dict()
+    with open(csv_file, 'r', newline='') as _:
+        reader = csv.reader(_)
+        # filename, sample, group
+        for row in reader:
+            filename, sample, group = row
+            if sample.lower() == 'negative':
+                sample = 'Negative'
+            if sample.lower() == 'positive':
+                sample = 'Positive'
+            data[sample] = [filename, group]
+    return data
+
+
+def get_data(filename: str, neg=0, pos=255) -> np.array:
+    img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+    if img is None:
+        raise ValueError(f'Bad input file {filename}')
+    b, g, r, a = cv2.split(img)
+    # exclude background
+    b[a<255] = 255
+    b_r = 255 - b
+    # apply negative adjustment
+    # cv2.imshow('a', b_r)
+    b_r[b_r<neg] = 0
+    # cv2.waitKey()
+    # exclude negative region
+    data = b_r[a>0]
+    print(data.shape)
+    print(data.mean())
+    return data
+
+
+def main():
+    info = get_sample_info(argv[1])
+    if 'negative' not in info and 'Negative' not in info:
+        raise ValueError('Please add negative reference in csv file!')
+    if 'positive' not in info and 'Positive' not in info:
+        raise ValueError('Please add positive reference in csv file!')
+    negative = get_data(info['Negative'][0])
+    positive = get_data(info['Positive'][0])
+    neg_mean = int(np.mean(negative))
+    pos_mean = int(np.mean(positive))
+    data = dict()
+    for sample in info:
+        if sample in ('Negative', 'Positive'):
+            continue
+        data[sample] = get_data(info[sample][0], neg_mean, pos_mean)
+    v = plt.violinplot(list(data.values()), showmeans=False, showmedians=False,
+                       showextrema=False)
+    plt.yticks(np.linspace(0, 256, 9))
+    plt.show()
+    # cv2.imshow('raw', img)
+    # cv2.waitKey()
+    return
+
+
+main()
