d0497dc8985ff5f4d33dc9033d155ef46df21331
Ping Wu <wpwupingwp@outlook.com>
2024-11-20 09:49:02 +0800

    try to rebuild

--- build.ps1
+++ build.ps1
@@ -4,25 +4,25 @@
 # rm -r -Force build_venv
 python3 -m venv build_venv
 build_venv/Scripts/activate.ps1
-uv pip uninstall pygus-bio
+pip uninstall pygus-bio
 # Some of these packages are only used for build, so they are not listed in
 # requirement.txt
 # higher version of niutka failed on multiprocessing
 # uv pip install Nuitka==1.0.7
-uv pip install -U Nuitka
+pip install -U Nuitka
 #uv pip install Nuitka==1.2
-uv pip install colour-science==0.4.4
+pip install colour-science>=0.4.4
 # uv pip install imageio==2.26.0
-uv pip install orderedset
-uv pip install coloredlogs==15.0.1
-uv pip install matplotlib==3.5.2
+pip install orderedset
+pip install coloredlogs>=15.0.1
+pip install matplotlib>=3.5.2
 # old version of scipy require old numpy
-uv pip install numpy==1.22.4
-uv pip install opencv-contrib-python==4.6.0.66
+pip install numpy==1.22.4
+pip install opencv-contrib-python>=4.6.0.66
 # in windows 1.7.2 works
-uv pip install scipy==1.8
+pip install scipy>=1.8
 # uv pip install scipy==1.7.2
-uv pip install tqdm==4.64.1
+pip install tqdm>=4.64.1
 pwd
 nuitka --standalone --onefile --enable-plugin=tk-inter src/pyGUS --windows-icon-from-ico=icon.ico --include-data-dir=./src/pyGUS/data=pyGUS/data
 # mac
